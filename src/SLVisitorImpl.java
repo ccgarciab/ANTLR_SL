eccf31809a48f48fed4def743704c54ad23093b6
santiago <santiagohernandez@seven4n.com>
2022-05-29 12:40:49 -0500

    visitPrimario done and visit IDENTIFICADOR as an auxiliar method created in SLVisitorImpl.java

--- src/SLVisitorImpl.java
+++ src/SLVisitorImpl.java
@@ -1,7 +1,45 @@
 import java.util.HashMap;
 
+import org.antlr.v4.runtime.tree.TerminalNode;
+
 public class SLVisitorImpl<T> extends SLBaseVisitor<T> {
 
     HashMap<String,Object> values = new HashMap<>();
 
+    @Override public T visitPotencia(SLParser.PotenciaContext ctx) {
+
+        return visitChildren(ctx);
+    }
+
+    @Override public T visitAcceso(SLParser.AccesoContext ctx){
+        return null;
+    }
+
+    @Override public T visitPrimario(SLParser.PrimarioContext ctx){
+        if (ctx.literal() != null) return visitLiteral(ctx.literal());
+        else if (ctx.IDENTIFICADOR()!= null) {
+            return visitIdentificador(ctx.IDENTIFICADOR());
+        }else {
+            return visitExpr(ctx.expr());
+        }
+
+    }
+
+    @Override public T visitArgumentos(SLParser.ArgumentosContext ctx){
+        return null;
+    }
+
+    public T visitIdentificador(TerminalNode ctx){
+        String name = ctx.getText();
+        Object value;
+        if ((value = values.get(name)) == null) {
+            int line = ctx.getSymbol().getLine();
+            int col = ctx.getSymbol().getCharPositionInLine()+1;
+            System.err.printf("<%d:%d> Error semantico, la variable con nombre \"" + name + "\" no fue declarada.\n", line, col);
+            System.exit(-1);
+            return null;
+        } else {
+            return (T) value;
+        }
+    }
 }
