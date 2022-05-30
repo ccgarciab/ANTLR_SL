2b9e1568209c3558d4ef88a35d3a5501a2d59f20
santiago <santiagohernandez@seven4n.com>
2022-05-30 08:51:31 -0500

    visit argumentos

--- src/SLVisitorImpl.java
+++ src/SLVisitorImpl.java
@@ -65,6 +65,11 @@ public class SLVisitorImpl<T> extends SLBaseVisitor<T> {
     }
 
     @Override public T visitArgumentos(SLParser.ArgumentosContext ctx){
+        visitExpr(ctx.expr(0));
+        int i = 1;
+        while (ctx.expr(i)!=null){
+            visitExpr(ctx.expr(i));
+        }
         return null;
     }
 
