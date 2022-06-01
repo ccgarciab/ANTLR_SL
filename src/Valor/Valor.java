18966558ed64a68eaafb0bf40022cd53996552a3
ccgarciab <ccgarciab@unal.edu.co>
2022-06-01 10:07:16 -0500

    Completed class Valor

--- src/Valor/Valor.java
+++ src/Valor/Valor.java
@@ -4,6 +4,14 @@ import Tipo.Tipo;
 
 public class Valor {
 
-    Tipo tipo;
-    Object valor;
+    public Tipo tipo;
+    public boolean constante;
+    public Object valor;
+
+
+    public Valor(Tipo tipo, boolean constante, Object valor){
+        this.tipo = tipo;
+        this.constante = constante;
+        this.valor = valor;
+    }
 }
